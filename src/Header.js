a5a302a175e17e50591133d1f9f7d87aa0e963d0
Tyler Buchea <tyler@buchea.com>
2016-12-21 15:13:28 -0800

    :tada: Official build v1 sauwce

--- src/Header.js
+++ src/Header.js
@@ -73,9 +73,6 @@ export default class Header extends Component {
                 <i className="fa fa-calendar"></i> <a href="https://calendly.com/colbycurtis" onClick={() => window.ga('send', 'event', 'Contact Request', 'Calendly', 'Tried to schedule a calendly meeting')}>Schedule a meeting</a>
               </li>
               <li>
-                <i className="fa fa-twitter"></i> <a href="https://twitter.com/colbycurtis19" target="_blank" onClick={() => window.ga('send', 'event', 'Contact Request', 'Twitter', 'Tried to contact me via Twitter')}>Tweet at me</a>
-              </li>
-              <li>
                 <i className="fa fa-envelope"></i> <a href="mailto:colby@colbycurtis.fitness" target="_blank" onClick={() => window.ga('send', 'event', 'Contact Request', 'Email', 'Tried to contact me via email')}>Send me an email</a>
               </li>
               <li>
@@ -92,11 +89,16 @@ export default class Header extends Component {
           <article className="content-item content-direct">
             <div className="about">
               <div className="about-copy">
-                <h2 className="title is-2">Howdy,</h2>
-                <p>
-                  I'm Tyler Buchea, I'm a full stack JavaScript developer working
-                  and living in the Portland area. I like React, React Native,
-                  Redux, Express, NodeJS, GraphQL, and Relay. Add me on Snapchat: <strong>tylerbuchea</strong>.
+                <h2 className="title is-2">Welcome,</h2>
+                <p style={{ marginRight: 10 }}>
+                  I’m Colby Curtis, a NASM certified fitness professional with
+                  a desire to help you along your path to a healthier you.
+                  Health isn’t just having a self installed washboard or arms
+                  that don’t allow for sleeved shirts, it’s about being
+                  physically and mentally energetic while living a satisfied
+                  and happy life. All anyone really wants is to find happiness,
+                  and I believe fitness done the right way for the right
+                  reasons to be an excellent tool to help accomplish that goal.
                 </p>
               </div>
               <img src="img/icon-avatar.png" className="icon-avatar" role="presentation" />
